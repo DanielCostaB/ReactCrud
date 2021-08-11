77f6950d6395fa03fab8d289372e60b0eb8fd3f7
Zartdc <daniel.c.dan@gmail.com>
2021-08-11 15:26:18 -0300

    React arquivos iniciais crud com firebase

---
+++ src/components/ContactForm.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const ContactForm = () => {
+  return (
+    <div>display form</div>
+  );
+}
+
+export default ContactForm;
---
+++ src/firebase.js
@@ -0,0 +1,23 @@
+import * as firebase from "firebase";
+
+// Your web app's Firebase configuration
+
+var firebaseConfig = {
+
+  apiKey: "AIzaSyDlprNYXSXCeosZqxKi36oGhbgnKcRZHpw",
+
+  authDomain: "react-crud-b86cf.firebaseapp.com",
+
+  projectId: "react-crud-b86cf",
+
+  storageBucket: "react-crud-b86cf.appspot.com",
+
+  messagingSenderId: "4304375400",
+
+  appId: "1:4304375400:web:05971df7c31a0e358d35dd"
+
+};
+
+// Initialize Firebase
+
+firebase.initializeApp(firebaseConfig);
